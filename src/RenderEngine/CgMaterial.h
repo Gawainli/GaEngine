363f0ea31240e22bc983afc58fb3fa177c3bb6e1
Gawain Li <gawainli@outlook.com>
2013-07-26 10:12:36 +0800

    add CgMaterial files

---
+++ src/RenderEngine/CgMaterial.h
@@ -0,0 +1,19 @@
+#ifndef CgMaterial_h__
+#define CgMaterial_h__
+
+#include "common.h"
+#include "EngineDef.h"
+
+ENGINE_NS_BEGIN
+
+class CgMaterial
+{
+public:
+	CgMaterial();
+	virtual ~CgMaterial();
+protected:
+private:
+};
+
+ENGINE_NS_END
+#endif // CgMaterial_h__
